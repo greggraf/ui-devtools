1c375730733d7ac3db50c7187015763d2e5a5f9c
Graf, Greg <greg.graf@viacom.com>
2014-12-12 12:54:12 -0500

    add git and only load node if its not there

--- script.sh
+++ script.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
  
-echo "Provisioning virtual machine... for packer"
+echo "Provisioning virtual machine"
 
 echo "Update apt-get"
-sudo add-apt-repository ppa:chris-lea/node.js 
 sudo apt-get update 
 
-echo "Installing curl, htop, build essentials, openssl, libssl-dev, pkg-config"
+echo "Installing curl, htop, build essentials, openssl, libssl-dev, pkg-config, git-core"
 sudo apt-get install curl -y
 sudo apt-get install htop -y
 sudo apt-get install build-essential openssl libssl-dev pkg-config -y
-
+sudo apt-get install git-core -y
 
 
 echo "nodejs"
 
-cd /usr/local/src
-sudo mkdir node
-cd node
-sudo wget http://nodejs.org/dist/v0.10.33/node-v0.10.33.tar.gz
-sudo tar -xzf node-v0.10.33.tar.gz 
-cd node-v0.10.33/
-sudo ./configure
-sudo make
-sudo make install
-
-
+if ! type "node" > /dev/null; then 
+  cd /usr/local/src
+  sudo mkdir node
+  cd node
+  sudo wget http://nodejs.org/dist/v0.10.33/node-v0.10.33.tar.gz
+  sudo tar -xzf node-v0.10.33.tar.gz 
+  cd node-v0.10.33/
+  sudo ./configure
+  sudo make
+  sudo make install
+fi

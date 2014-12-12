6ab276dcd4f8a88dcc5e9628aae608a3a20eadb0
Graf, Greg <greg.graf@viacom.com>
2014-12-12 13:22:41 -0500

    adding phantomjs

--- script.sh
+++ script.sh
@@ -5,11 +5,13 @@ echo "Provisioning virtual machine"
 echo "Update apt-get"
 sudo apt-get update 
 
-echo "Installing curl, htop, build essentials, openssl, libssl-dev, pkg-config, git-core"
+echo "Installing curl, htop, build essentials, openssl, libssl-dev, pkg-config, git-core, libfontconfig, libfontconfig-dev, libfreetype6-dev"
 sudo apt-get install curl -y
 sudo apt-get install htop -y
 sudo apt-get install build-essential openssl libssl-dev pkg-config -y
 sudo apt-get install git-core -y
+
+sudo apt-get install libfontconfig libfontconfig-dev libfreetype6-dev -y
 
 
 echo "nodejs"
@@ -24,4 +26,13 @@ if ! type "node" > /dev/null; then
   sudo ./configure
   sudo make
   sudo make install
+fi
+
+echo "phantomjs"
+
+if ! type "phantomjs" > /dev/null; then 
+  cd /opt
+  wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
+  sudo tar -xvf phantomjs-1.9.2-linux-x86_64.tar.bz2
+  sudo ln -s /opt/phantomjs-1.9.2-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
 fi

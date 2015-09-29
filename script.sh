cf7a51247b7fc79802f197686ff6e2a753768581
Graf, Greg <greg.graf@viacom.com>
2015-09-29 13:35:53 -0400

    update code for installing Docker, install NVM instead of Node directly so we can switch between versions of NodeJS more easily

--- script.sh
+++ script.sh
@@ -6,8 +6,10 @@ echo "Update apt-get"
 sudo apt-get update
 
 echo "Install Docker and add vagrant to the docker group"
-if ! type "docker" > /dev/null; then
-	wget -qO- https://get.docker.com/ | sh
+
+if ! command -v docker >/dev/null 2>&1; then
+	echo "Docker not installed"
+	sudo curl -sSL https://get.docker.com/ | sh
 fi
 
 usermod -aG docker vagrant
@@ -25,20 +27,6 @@ sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update
 sudo apt-get install git -y
 
-echo "nodejs"
-
-if ! type "node" > /dev/null; then
-  cd /usr/local/src
-  sudo mkdir node
-  cd node
-  sudo wget http://nodejs.org/dist/v0.10.35/node-v0.10.35.tar.gz
-  sudo tar -xzf node-v0.10.35.tar.gz
-  cd node-v0.10.35/
-  sudo ./configure
-  sudo make
-  sudo make install
-fi
-
 echo "phantomjs"
 
 if ! type "phantomjs" > /dev/null; then
@@ -48,10 +36,6 @@ if ! type "phantomjs" > /dev/null; then
   sudo ln -s /opt/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
 fi
 
-echo "add node_modules/.bin to path"
-echo 'export PATH=$PATH:node_modules/.bin' >> /home/vagrant/.profile
-echo 'export PATH=$PATH:node_modules/.bin' >> /home/vagrant/.bash_profile
-
 echo "apache, jump on it"
 mv /home/vagrant/files/arc-ui_apache /etc/apache2/sites-available/arc-ui
 rm -f /etc/apache2/sites-enabled/arc-ui && ln -s /etc/apache2/sites-available/arc-ui /etc/apache2/sites-enabled/arc-ui
@@ -59,8 +43,6 @@ rm -f /etc/apache2/sites-enabled/arc-ui && ln -s /etc/apache2/sites-available/ar
 
 echo "notify-send"
 sudo apt-get install libnotify-bin -y
-
-
 
 # this should probably be last
 echo 'clean up /home/vagrant/files/'
